59c8097d0470e0557e1f8c52c870935e9aa20e10
Claire Ituin <Claire Ituin>
2016-05-27 14:48:04 +0200

    saved everything or something

--- js/spaceinvaders.js
+++ js/spaceinvaders.js
@@ -566,12 +566,7 @@ PlayState.prototype.draw = function(game, dt, ctx) {
     ctx.clearRect(0, 0, game.width, game.height);
     
     //  Draw ship.
-<<<<<<< HEAD
     ctx.fillStyle = '#00FFFF';
-=======
-    //ctx.fillStyle = '#999999';
-	ctx.fillStyle = '#'+333333*game.lives;
->>>>>>> cea9d83b0614eb652d31065918b0c3bcf3e437b6
     ctx.fillRect(this.ship.x - (this.ship.width / 2), this.ship.y - (this.ship.height / 2), this.ship.width, this.ship.height);
 
     //  Draw invaders.
